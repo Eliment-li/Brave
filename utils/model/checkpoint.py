ce11b002181f06b32eabc0221110be83056ab645
Eliment-li <felix.litian@foxmail.com>
2025-12-02 22:56:26 +0800

    update checkpoint saving path to use Path for better compatibility and add weights_only option in load_checkpoint

--- utils/model/checkpoint.py
+++ utils/model/checkpoint.py
@@ -1,11 +1,14 @@
 import os
 import time
+from pathlib import Path
 
 import torch
 
+from configs.args import get_root_path
+
 
 def save_checkpoint(run_name, iteration, global_step, agent, optimizer, args):
-    ckpt_dir = os.path.join("checkpoints", run_name)
+    ckpt_dir = Path(get_root_path())/"results"/"checkpoints"/run_name
     os.makedirs(ckpt_dir, exist_ok=True)
     ckpt_path = os.path.join(ckpt_dir, f"checkpoint_iter{iteration}_step{global_step}.pt")
     try:
@@ -26,7 +29,7 @@ def save_checkpoint(run_name, iteration, global_step, agent, optimizer, args):
     print(f"Saved checkpoint: {ckpt_path}")
 
 def load_checkpoint(ckpt_path, agent, optimizer, map_location=None):
-    ckpt = torch.load(ckpt_path, map_location=map_location)
+    ckpt = torch.load(ckpt_path, map_location=map_location,weights_only=False)
     agent.load_state_dict(ckpt["model_state_dict"])
     optimizer.load_state_dict(ckpt["optimizer_state_dict"])
     return ckpt["iteration"], ckpt["global_step"], ckpt.get("args")
